ac28b62305fda2071c53cb8bec54e0c6dabbc2dc
Mathieu Rodrigues <Mathieu Rodrigues>
2016-06-19 19:38:45 +0200

    Ajout de la page 404 (vue + css)

---
+++ www/views/404.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <?php include "include/head.inc.php" ?>
+    <title>Fame on - La télé-réalité comme vous ne l'avez jamais vu</title>
+    <meta name="description" content="C'est bien d'être arrivé jusqu'ici... Mais ca ne sert finalement à rien, si ce n'est pour voir cette magnifique page 404.">
+</head>
+
+<body id="error-404">
+
+    <main class="bloc-404 col-xs-12 col-md-5">
+      <h1>C'est bien d'être arrivé jusqu'ici... Mais ca ne sert finalement à rien, si ce n'est pour voir cette <s>magnifique</s> page 404.</h1>
+      <a href="<?php $this->utils->generateUrl("/"); ?>" title="Retournez à la page d'accueil">Retournez à la page d'accueil</a>
+    </main>
+
+</body>
+
+</html>
