34f6c47e49fd081394539c050e73bc4f9ef331fc
Mathieu Rodrigues <Mathieu Rodrigues>
2016-06-19 20:40:19 +0200

    Ajout d'une page 404

--- www/views/404.php
+++ www/views/404.php
@@ -2,17 +2,28 @@
 <html lang="fr">
 
 <head>
-  <?php include "include/head.inc.php" ?>
-    <title>Fame on - La télé-réalité comme vous ne l'avez jamais vu</title>
+  <meta charset="UTF-8">
+  <meta name="robots" content="index, follow">
+  <meta name="author" content="Views IESA">
+  <link rel="icon" href="../res/img/favicon.png">
+  <!--[if lt IE 9]>
+    <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+  <meta name="viewport" content="width=device-width">
+  <!-- ************** STYLESHEET ************** -->
+  <link rel="stylesheet" href="../res/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../res/css/main.css">
+  <!-- ************ END STYLESHEET ************ -->
+    <title>Fame on - La page qui ne mène à rien...</title>
     <meta name="description" content="C'est bien d'être arrivé jusqu'ici... Mais ca ne sert finalement à rien, si ce n'est pour voir cette magnifique page 404.">
 </head>
 
 <body id="error-404">
 
-    <main class="bloc-404 col-xs-12 col-md-5">
-      <h1>C'est bien d'être arrivé jusqu'ici... Mais ca ne sert finalement à rien, si ce n'est pour voir cette <s>magnifique</s> page 404.</h1>
-      <a href="<?php $this->utils->generateUrl("/"); ?>" title="Retournez à la page d'accueil">Retournez à la page d'accueil</a>
-    </main>
+  <main class="bloc-404 col-xs-12 col-md-5">
+    <h1>C'est bien d'être arrivé jusqu'ici... Mais ca ne sert finalement à rien, si ce n'est pour voir cette <s>magnifique</s> page 404.</h1>
+    <a href="/" title="Retournez à la page d'accueil">Retournez à la page d'accueil</a>
+  </main>
 
 </body>
 
