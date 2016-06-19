74c3e59b427715af85d35b1343a1988922341215
Mathieu Rodrigues <mathieu.rodrigues17@gmail.com>
2016-06-19 07:30:45 +0200

    Màj des include, gabarit de page formaté

---
+++ www/views/include/footer.inc.php
@@ -0,0 +1,3 @@
+<?php ?>
+<script src='res/js/main.js'></script>
+<!--<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-575e0e0e8a91eeed"></script>-->
---
+++ www/views/include/head.inc.php
@@ -0,0 +1,16 @@
+<?php
+?>
+
+<meta charset="UTF-8">
+<meta name="robots" content="index, follow">
+<meta name="author" content="Views IESA">
+<link rel="icon" href="res/img/favicon.png">
+<!--[if lt IE 9]>
+    <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+<meta name="viewport" content="width=device-width">
+<!-- ************** STYLESHEET ************** -->
+<link rel="stylesheet" href="res/css/bootstrap.min.css">
+<link rel="stylesheet" href="res/css/main.css">
+<!-- ************ END STYLESHEET ************ -->
+<?php include __DIR__.'/analyticstracking.inc.php' ?>
