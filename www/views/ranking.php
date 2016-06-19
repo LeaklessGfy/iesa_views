ae5d75cbc2ffd0a7de22ead9dab6243ea0420500
Mathieu Rodrigues <mathieu.rodrigues17@gmail.com>
2016-06-19 07:35:13 +0200

    Création des pages annexes

---
+++ www/views/ranking.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+    <!DOCTYPE html>
+    <html lang="fr">
+
+    <head>
+        <?php include "/include/head.inc.php" ?>
+            <title>Fame on - La télé-réalité comme vous ne l'avez jamais vu</title>
+            <meta name="description" content="">
+    </head>
+
+    <body class="container">
+        <?php include("/include/menu.inc.php"); ?>
+
+
+
+            <?php include("/include/footer.inc.php"); ?>
+    </body>
+
+    </html>
