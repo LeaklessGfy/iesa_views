433a5550abcbd65ce8b998188cbf0be9e0a4c783
Mathieu Rodrigues <Mathieu Rodrigues>
2016-07-01 00:00:20 +0200

    Ajout du player video sur la première page d'index, ajout des images before et after de certains bloc, ajustement style page index 1

--- www/views/index.php
+++ www/views/index.php
@@ -12,20 +12,14 @@
 
     <section id="concept" class="container">
       <div class="row">
-        <div class="block-logo col-xs-12 col-md-6">
-          
-        </div>
+        <div class="block-logo col-xs-12 col-md-6"></div>
         <div class="what-is-it col-xs-12 col-md-6">
-          <h2><img src="res/img/c-est-quoi.png" alt="C'est quoi ?"></h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+          <h2 class=" col-md-8 col-md-offset-2"><img src="res/img/c-est-quoi.png" alt="C'est quoi ?"></h2>
+          <p class="col-md-8 col-md-offset-2 add-border-before add-arrow-after">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
         </div>
       </div>
       <div class="row">
-        <div id="player-youtube">
-          <div class="embed-container">
-            <iframe src='http://www.youtube.com/embed/qJ_Tw0w3lLA' frameborder='0' allowfullscreen></iframe>
-          </div>
-        </div>
+        <video controls src="res/videos/concept.mp4">Vidéo motion descriptive du concept de l'émission Fame On.</video>
       </div>
     </section>
 
