86d1057901d0532ed83aeefdb19d66e087914b23
Mathieu Rodrigues <mathieu.rodrigues17@gmail.com>
2016-07-03 17:04:11 +0200

    Ajout mentions légales img

--- www/views/legal_notice.php
+++ www/views/legal_notice.php
@@ -11,8 +11,8 @@
   <?php include("include/menu.inc.php"); ?>
     <main class="container">
 
-      <h2 class="text-center">Mentions légales</h2>
-      
+      <h1 class="text-center"><img src="res/img/mentions-legales.png" alt="Mentions légales"></h1>
+
       <section class="col-xs-12">
         <h3>1. Présentation du site.</h3>
         <p>En vertu de l'article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique, il est précisé aux utilisateurs du site <a href="http://www.fame-on.fr/" title="Views - www.fame-on.fr">www.fame-on.fr</a> l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi :</p>
